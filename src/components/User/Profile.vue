c713a38ee9314394792b02ddabd3d9f829765318
crab777 <baiborodin00@gmail.com>
2020-02-14 04:05:16 +0200

    Add user profile

---
+++ src/components/User/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="user-profile"> <!--Обязательный корневой элемент-->
+        <h1>User profile</h1>
+        <p><strong>Name</strong>{{ user.name }}</p>
+        <p><strong>Surname</strong>{{ user.surname }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    data() {
+        return {
+            user: {
+                name: 'John',
+                surname: 'Doe'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
